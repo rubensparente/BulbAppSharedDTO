7a4d16c61be0736fcb75cc76ccabf3bc6c2b5236
rubensparente <rubensparente@yahoo.com.br>
2024-10-02 19:34:28 -0300

    Update TurmaRequestDTO.swift

--- Sources/BulbAppSharedDTO/TurmaRequestDTO.swift
+++ Sources/BulbAppSharedDTO/TurmaRequestDTO.swift
@@ -12,12 +12,15 @@ public struct TurmaRequestDTO: Codable{
     public let nome: String
     public let dataInicio: Date?
     public let dataFinal: Date?
+    public let codigoTurma: Int
     public let listaAlunos: [String]
     
-    public init(nome: String, dataInicio: Date?, dataFinal: Date?, listaAlunos: [String]) {
+    
+    public init(nome: String, dataInicio: Date?, dataFinal: Date?, codigoTurma: Int, listaAlunos: [String]) {
         self.nome = nome
         self.dataInicio = dataInicio
         self.dataFinal = dataFinal
+        self.codigoTurma = codigoTurma
         self.listaAlunos = listaAlunos
     }
 }
